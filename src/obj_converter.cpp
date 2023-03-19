e926aa4b317081b7afbd42b7e51d6041376c87f4
ms-elk <masamichi.sugihara@gmail.com>
2023-03-19 14:32:58 +0900

    fix obj_converter to load .mtl file

--- src/obj_converter.cpp
+++ src/obj_converter.cpp
@@ -3,6 +3,8 @@
 #include "lambertian.h"
 #include "mesh.h"
 
+#include <filesystem>
+
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tinyobjloader/tiny_obj_loader.h>
 
@@ -15,12 +17,29 @@ createMesh(const char* filename)
     std::vector<tinyobj::material_t> objMaterials;
     std::string warn;
     std::string err;
-    bool loaded = tinyobj::LoadObj(&attrib, &objShapes, &objMaterials, &warn, &err, filename, nullptr, true);
+    std::filesystem::path filepath(filename);
+    filepath.remove_filename();
+    bool loaded = tinyobj::LoadObj(&attrib, &objShapes, &objMaterials, &warn, &err,
+        filename, filepath.string().c_str(), true);
+
+    if (!err.empty())
+    {
+        printf("%s", err.c_str());
+        return nullptr;
+    }
+
+    if (!warn.empty())
+    {
+        printf("%s", warn.c_str());
+        return nullptr;
+    }
+
     if (!loaded)
     {
         return nullptr;
     }
 
+    // extract materials from Obj
     std::vector<std::shared_ptr<Material>> materials;
     materials.reserve(objMaterials.size());
     for (const auto& om : objMaterials)
@@ -31,10 +50,12 @@ createMesh(const char* filename)
 
     auto meshData = std::make_shared<Mesh::Data>();
 
+    // copy vertices from Obj
     size_t vertexCount = attrib.vertices.size() / 3;
     meshData->vertices.resize(vertexCount);
     memcpy(meshData->vertices.data(), attrib.vertices.data(), sizeof(float3) * vertexCount);
 
+    // assign prim indices and material IDs
     for (const auto& shape : objShapes)
     {
         const auto& sm = shape.mesh;
@@ -42,7 +63,7 @@ createMesh(const char* filename)
 
         for (int i = 0; i < primCount; ++i)
         {
-            const auto* index = &sm.indices[3*i];
+            const auto* index = &sm.indices[3 * i];
             meshData->prims.push_back(uint3(index[0].vertex_index, index[1].vertex_index, index[2].vertex_index));
 
             meshData->materials.push_back(materials[sm.material_ids[i]]);
