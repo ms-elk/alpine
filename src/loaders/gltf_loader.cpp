a457e4896bae95a1a0ddfbf3d0cfde9e2901b9ca
ms-elk <masamichi.sugihara@gmail.com>
2024-08-06 19:50:05 +0900

    add light supoort to gltf loader

--- src/loaders/gltf_loader.cpp
+++ src/loaders/gltf_loader.cpp
@@ -1,5 +1,6 @@
 #include "file_loader.h"
 
+#include "lights/point_light.h"
 #include "materials/matte.h"
 #include "materials/metal.h"
 #include "math/matrix.h"
@@ -27,9 +28,9 @@ public:
     bool load(std::string_view filename);
 
 private:
-    void traverse(float4x4 matrix, uint32_t nodeIdx);
+    void traverse(const float4x4& parent, uint32_t nodeIdx);
 
-    std::shared_ptr<Texture4f> createTexture(uint32_t texIdx, bool isSigned);
+    std::shared_ptr<Mesh> createMesh(const float4x4& matrix, uint32_t meshIdx) const;
 
     template <typename T>
     std::size_t appendVertexBuffer(
@@ -42,6 +43,12 @@ private:
         const tinygltf::Primitive& srcPrim) const;
 
     std::size_t appendIndexBuffer(std::vector<uint3>& dst, const tinygltf::Primitive& srcPrim) const;
+
+    std::shared_ptr<Material> createMaterial(uint32_t matIdx) const;
+
+    std::shared_ptr<Texture4f> createTexture(uint32_t texIdx, bool isSigned) const;
+
+    std::shared_ptr<Light> createLight(const float4x4& matrix, uint32_t lightIdx) const;
 
 private:
     Scene* mScene = nullptr;
@@ -76,34 +83,9 @@ GltfLoader::load(std::string_view filename)
 
     // extract materials
     mMaterials.reserve(mSrcModel.materials.size());
-    for (const auto& srcMat : mSrcModel.materials)
+    for (uint32_t matIdx = 0; matIdx < mSrcModel.materials.size(); ++matIdx)
     {
-        std::shared_ptr<Texture4f> baseColorTex = nullptr;
-        if (int32_t texIdx = srcMat.pbrMetallicRoughness.baseColorTexture.index; texIdx >= 0)
-        {
-            baseColorTex = createTexture(texIdx, false);
-        }
-
-        std::shared_ptr<Texture4f> normalTex = nullptr;
-        if (int32_t texIdx = srcMat.normalTexture.index; texIdx >= 0)
-        {
-            normalTex = createTexture(texIdx, true);
-        }
-
-        const auto& d = srcMat.pbrMetallicRoughness.baseColorFactor;
-        float3 baseColor(static_cast<float>(d[0]), static_cast<float>(d[1]), static_cast<float>(d[2]));
-
-        if (srcMat.pbrMetallicRoughness.metallicFactor > 0.0)
-        {
-            float roughness = static_cast<float>(srcMat.pbrMetallicRoughness.roughnessFactor);
-            mMaterials.push_back(std::make_shared<Metal>(
-                float2(roughness), baseColor, baseColorTex, normalTex));
-        }
-        else
-        {
-            mMaterials.push_back(std::make_shared<Matte>(
-                baseColor, baseColorTex, normalTex));
-        }
+        mMaterials.push_back(createMaterial(matIdx));
     }
 
     for (const auto& srcScene : mSrcModel.scenes)
@@ -178,58 +160,20 @@ getMatrix(const tinygltf::Node& node)
 }
 
 void
-GltfLoader::traverse(float4x4 matrix, uint32_t nodeIdx)
+GltfLoader::traverse(const float4x4& parent, uint32_t nodeIdx)
 {
     const auto& srcNode = mSrcModel.nodes[nodeIdx];
 
-    matrix = mul(matrix, getMatrix(srcNode));
+    float4x4 matrix = mul(parent, getMatrix(srcNode));
 
     if (srcNode.mesh >= 0)
     {
-        Mesh::Data meshData;
-        const auto& srcMesh = mSrcModel.meshes[srcNode.mesh];
+        mScene->shapes.push_back(createMesh(matrix, srcNode.mesh));
+    }
 
-        for (const auto& srcPrim : srcMesh.primitives)
-        {
-            assert(srcPrim.mode == TINYGLTF_MODE_TRIANGLES);
-
-            appendVertexBuffer(meshData.vertices, "POSITION", srcPrim);
-            appendVertexBuffer(meshData.normals, "NORMAL", srcPrim);
-            appendVertexBuffer(meshData.uvs, "TEXCOORD_0", srcPrim);
-
-            appendTangentBuffer(
-                meshData.tangents, meshData.bitangents, meshData.normals, srcPrim);
-
-            std::size_t primCount = appendIndexBuffer(meshData.prims, srcPrim);
-            for (uint32_t i = 0; i < primCount; ++i)
-            {
-                meshData.materials.push_back(mMaterials[srcPrim.material]);
-            }
-        }
-
-        // transform
-        // TODO: fix matrix for normals
-        assert(meshData.vertices.size() == meshData.normals.size());
-        for (uint32_t i = 0; i < meshData.vertices.size(); ++i)
-        {
-            auto& v = meshData.vertices[i];
-            v = mul(matrix, float4(v.x, v.y, v.z, 1.0f)).xyz();
-
-            auto& n = meshData.normals[i];
-            n = mul(matrix, float4(n.x, n.y, n.z, 0.0f)).xyz();
-
-            if (!meshData.tangents.empty())
-            {
-                auto& t = meshData.tangents[i];
-                t = mul(matrix, float4(t.x, t.y, t.z, 0.0f)).xyz();
-
-                assert(!meshData.bitangents.empty());
-                auto& b = meshData.bitangents[i];
-                b = mul(matrix, float4(b.x, b.y, b.z, 0.0f)).xyz();
-            }
-        }
-
-        mScene->shapes.push_back(std::make_shared<Mesh>(std::move(meshData)));
+    if (srcNode.light >= 0)
+    {
+        mScene->lights.push_back(createLight(matrix, srcNode.light));
     }
 
     for (uint32_t childIdx : srcNode.children)
@@ -238,41 +182,54 @@ GltfLoader::traverse(float4x4 matrix, uint32_t nodeIdx)
     }
 }
 
-std::shared_ptr<Texture4f>
-GltfLoader::createTexture(uint32_t texIdx, bool isSigned)
+std::shared_ptr<Mesh>
+GltfLoader::createMesh(const float4x4& matrix, uint32_t meshIdx) const
 {
-    uint32_t imgIdx = mSrcModel.textures[texIdx].source;
-    const auto& image = mSrcModel.images[imgIdx];
+    Mesh::Data meshData;
+    const auto& srcMesh = mSrcModel.meshes[meshIdx];
 
-    if (image.component == 3 || image.component == 4)
+    for (const auto& srcPrim : srcMesh.primitives)
     {
-        std::vector<float4> texData(image.width * image.height);
-        for (uint32_t i = 0; i < texData.size(); ++i)
+        assert(srcPrim.mode == TINYGLTF_MODE_TRIANGLES);
+        assert(srcPrim.material >= 0);
+
+        appendVertexBuffer(meshData.vertices, "POSITION", srcPrim);
+        appendVertexBuffer(meshData.normals, "NORMAL", srcPrim);
+        appendVertexBuffer(meshData.uvs, "TEXCOORD_0", srcPrim);
+
+        appendTangentBuffer(
+            meshData.tangents, meshData.bitangents, meshData.normals, srcPrim);
+
+        std::size_t primCount = appendIndexBuffer(meshData.prims, srcPrim);
+        for (uint32_t i = 0; i < primCount; ++i)
         {
-            if (image.component == 3)
-            {
-                const uint8_t* d = &image.image[3 * i];
-                texData[i] = float4(d[0], d[1], d[2], 255) / 255.0f;
-            }
-            else
-            {
-                const uint8_t* d = &image.image[4 * i];
-                texData[i] = float4(d[0], d[1], d[2], d[3]) / 255.0f;
-            }
-
-            if (isSigned)
-            {
-                texData[i] = texData[i] * 2.0f - 1.0f;
-            }
+            meshData.materials.push_back(mMaterials[srcPrim.material]);
         }
+    }
 
-        return std::make_shared<Texture4f>(image.width, image.height, std::move(texData));
-    }
-    else
+    // transform
+    // TODO: fix matrix for normals
+    assert(meshData.vertices.size() == meshData.normals.size());
+    for (uint32_t i = 0; i < meshData.vertices.size(); ++i)
     {
-        printf("The channel count of %s is not valid\n", image.name.c_str());
-        return nullptr;
+        auto& v = meshData.vertices[i];
+        v = mul(matrix, float4(v.x, v.y, v.z, 1.0f)).xyz();
+
+        auto& n = meshData.normals[i];
+        n = mul(matrix, float4(n.x, n.y, n.z, 0.0f)).xyz();
+
+        if (!meshData.tangents.empty())
+        {
+            auto& t = meshData.tangents[i];
+            t = mul(matrix, float4(t.x, t.y, t.z, 0.0f)).xyz();
+
+            assert(!meshData.bitangents.empty());
+            auto& b = meshData.bitangents[i];
+            b = mul(matrix, float4(b.x, b.y, b.z, 0.0f)).xyz();
+        }
     }
+
+    return std::make_shared<Mesh>(std::move(meshData));
 }
 
 template <typename T>
@@ -366,6 +323,90 @@ GltfLoader::appendIndexBuffer(std::vector<uint3>& dst, const tinygltf::Primitive
     }
 
     return primCount;
+}
+
+std::shared_ptr<Material>
+GltfLoader::createMaterial(uint32_t matIdx) const
+{
+    const auto& srcMat = mSrcModel.materials[matIdx];
+
+    std::shared_ptr<Texture4f> baseColorTex = nullptr;
+    if (int32_t texIdx = srcMat.pbrMetallicRoughness.baseColorTexture.index; texIdx >= 0)
+    {
+        baseColorTex = createTexture(texIdx, false);
+    }
+
+    std::shared_ptr<Texture4f> normalTex = nullptr;
+    if (int32_t texIdx = srcMat.normalTexture.index; texIdx >= 0)
+    {
+        normalTex = createTexture(texIdx, true);
+    }
+
+    const auto& d = srcMat.pbrMetallicRoughness.baseColorFactor;
+    float3 baseColor(static_cast<float>(d[0]), static_cast<float>(d[1]), static_cast<float>(d[2]));
+
+    if (srcMat.pbrMetallicRoughness.metallicFactor > 0.0)
+    {
+        float roughness = static_cast<float>(srcMat.pbrMetallicRoughness.roughnessFactor);
+        return std::make_shared<Metal>(
+            float2(roughness), baseColor, baseColorTex, normalTex);
+    }
+    else
+    {
+        return std::make_shared<Matte>(baseColor, baseColorTex, normalTex);
+    }
+}
+
+std::shared_ptr<Texture4f>
+GltfLoader::createTexture(uint32_t texIdx, bool isSigned) const
+{
+    uint32_t imgIdx = mSrcModel.textures[texIdx].source;
+    const auto& srcImage = mSrcModel.images[imgIdx];
+
+    if (srcImage.component == 3 || srcImage.component == 4)
+    {
+        std::vector<float4> texData(srcImage.width * srcImage.height);
+        for (uint32_t i = 0; i < texData.size(); ++i)
+        {
+            if (srcImage.component == 3)
+            {
+                const uint8_t* d = &srcImage.image[3 * i];
+                texData[i] = float4(d[0], d[1], d[2], 255) / 255.0f;
+            }
+            else
+            {
+                const uint8_t* d = &srcImage.image[4 * i];
+                texData[i] = float4(d[0], d[1], d[2], d[3]) / 255.0f;
+            }
+
+            if (isSigned)
+            {
+                texData[i] = texData[i] * 2.0f - 1.0f;
+            }
+        }
+
+        return std::make_shared<Texture4f>(srcImage.width, srcImage.height, std::move(texData));
+    }
+    else
+    {
+        printf("The channel count of %s is not valid\n", srcImage.name.c_str());
+        return nullptr;
+    }
+}
+
+std::shared_ptr<Light>
+GltfLoader::createLight(const float4x4& matrix, uint32_t lightIdx) const
+{
+    const auto& srcLight = mSrcModel.lights[lightIdx];
+    const auto& c = srcLight.color;
+    float3 intensity = float3(
+        static_cast<float>(c[0]),
+        static_cast<float>(c[1]),
+        static_cast<float>(c[2])) * static_cast<float>(srcLight.intensity);
+
+    float3 position = mul(matrix, float4(0.0f, 0.0f, 0.0f, 1.0f)).xyz();
+
+    return std::make_shared<PointLight>(intensity, position);
 }
 }
 
