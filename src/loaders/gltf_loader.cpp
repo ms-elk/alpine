90f10b163a085f7bd54b9cded4d7905f1b6e1a43
ms-elk <104879901+ms-elk@users.noreply.github.com>
2026-01-05 22:22:04 +0900

    remove define

--- src/loaders/gltf_loader.cpp
+++ src/loaders/gltf_loader.cpp
@@ -12,7 +12,6 @@
 #pragma warning(push)
 #pragma warning(disable:4018)
 #pragma warning(disable:4267)
-#define __STDC_LIB_EXT1__
 #define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
